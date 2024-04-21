b295dace1036e211d11719ce4cb5de2e6c7e6723
Muhammed Machrouh <th3justhacker@gmail.com>
2024-04-21 23:47:39 +0100

    Add import statement for sys in api/wsgi.py

--- api/wsgi.py
+++ api/wsgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
@@ -16,3 +17,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
 application = get_wsgi_application()
 
 app = application
+
+if os.environ.get('ENV') == 'production':
+    sys.path.append(os.path.dirname(os.path.dirname(__file__)))
