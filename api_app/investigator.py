6b65fb1b80594d8df1f7425bec0a8e97fa87f18a
Muhammed Machrouh <th3justhacker@gmail.com>
2024-05-28 11:05:20 +0100

    chore: Disable unused tools and update CVE search functionality

--- api_app/investigator.py
+++ api_app/investigator.py
@@ -20,23 +20,24 @@ import re
 load_dotenv(override=True)
 
 llm = Ollama(model="openhermes", base_url=os.getenv('OLLAMA_HOST'), temperature=0.3, num_predict=-1)
-wrn = Ollama(model="wrn", base_url=os.getenv('OLLAMA_HOST'))
-llama3 = Ollama(model="llama3", base_url=os.getenv('OLLAMA_HOST'), temperature=0.3)
+# wrn = Ollama(model="wrn", base_url=os.getenv('OLLAMA_HOST'))
+# llama3 = Ollama(model="llama3", base_url=os.getenv('OLLAMA_HOST'), temperature=0.3)
 
 
 cve_search_tool = CVESearchTool().cvesearch
+fetch_cve_tool = CVESearchTool().get_latest_cves
 misp_search_tool =  MispTool().search
 misp_search_by_date_tool = MispTool().search_by_date
 misp_search_by_event_id_tool = MispTool().search_by_event_id
-coder_tool = CoderTool().code_generation_tool
+# coder_tool = CoderTool().code_generation_tool Disabled for now for more stability
 
 get_technique_by_id = MitreTool().get_technique_by_id
 get_technique_by_name = MitreTool().get_technique_by_name
 get_malware_by_name = MitreTool().get_malware_by_name
 get_tactic_by_keyword = MitreTool().get_tactic_by_keyword
 
-tools = [cve_search_tool, misp_search_tool, misp_search_by_date_tool, misp_search_by_event_id_tool, 
-         coder_tool, get_technique_by_id, get_technique_by_name, get_malware_by_name, get_tactic_by_keyword]
+tools = [cve_search_tool, fetch_cve_tool, misp_search_tool, misp_search_by_date_tool, misp_search_by_event_id_tool, 
+         get_technique_by_id, get_technique_by_name, get_malware_by_name, get_tactic_by_keyword]
 
 # conversational agent memory
 memory = ConversationBufferWindowMemory(
--- api_app/tools/cve_avd_tool.py
+++ api_app/tools/cve_avd_tool.py
@@ -1,6 +1,34 @@
 import requests
 from langchain.tools import tool
+from langchain_community.llms import Ollama
 import json
+
+import sys
+import os
+
+from dotenv import load_dotenv
+
+from datetime import datetime, timedelta
+load_dotenv(override=True)
+
+llm = Ollama(model="openhermes", base_url=os.getenv('OLLAMA_HOST'), temperature=0.2, num_predict=4096, num_ctx=8192)
+
+def get_current_formatted_date():
+    # Get the current date and time
+    now = datetime.now()
+
+    # Format the date and time in the desired format
+    formatted_date = now.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3]
+    return formatted_date
+
+def get_yesterday_formatted_date():
+    # Get the current date and time
+    now = datetime.now()
+    # Calculate yesterday's date and time
+    yesterday = now - timedelta(days=2)
+    # Format the date and time in the desired format
+    formatted_date = yesterday.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3]
+    return formatted_date
 
 def format_cve(cve_response):
     """
@@ -12,7 +40,7 @@ def format_cve(cve_response):
     Returns:
     - A list of strings, each a formatted prompt for LLM based on the CVE entries in the response.
     """
-    formatted_prompts = "Use these CVE Search Results to answer user question:\n\n"
+    formatted_prompts = "CVE Search Results:\n\n"
     
     if len(cve_response['vulnerabilities']) == 0:
 
@@ -63,7 +91,8 @@ class CVESearchTool():
   def cvesearch(keyword: str, date: str = None):
     """
     Searches for CVEs based on a keyword or phrase and returns the results in JSON format.
-    
+    Use this when a user asks you about a certain CVE or a CVE related to a certain keyword. 
+    And not necesserly the latest CVEs.
     Parameters:
     - keyword (str): A word or phrase to search for in the CVE descriptions.
     - date (str): An optional date to include in the search query.
@@ -71,6 +100,7 @@ class CVESearchTool():
     Returns:
     - JSON: A list of CVEs matching the keyword search.
     """
+
     if date:
         keyword = f"{keyword} {date}"
     # Encode the spaces in the keyword(s) as "%20" for the URL
@@ -78,7 +108,7 @@ class CVESearchTool():
     # keyword_encoded = keyword_encoded.join(" 2023")
     
     # Construct the URL for the API request
-    url = f"https://services.nvd.nist.gov/rest/json/cves/2.0?keywordSearch={keyword_encoded}&resultsPerPage=5"
+    url = f"https://services.nvd.nist.gov/rest/json/cves/2.0?keywordSearch={keyword_encoded}&resultsPerPage=3"
     
     try:
         # Send the request to the NVD API
@@ -91,3 +121,33 @@ class CVESearchTool():
             return {"error": "Failed to fetch data from the NVD API.", "status_code": response.status_code}
     except Exception as e:
         return {"error": str(e)}
+
+  @tool("Get latest CVEs, today's CVEs Tool")
+  def get_latest_cves(keyword: str):
+        """
+        Fetches today's latest CVEs based on a keyword or phrase and returns the results in JSON format.
+        Use this exclusively to get the latest CVEs, or for todays CVEs. When the user asks you about the latest CVEs out there.
+        Parameters:
+        - keyword (str): A word or phrase to search for in the CVE descriptions.
+        Returns:
+        - A list of CVEs matching the keyword search.
+        """
+        # Encode the spaces in the keyword(s) as "%20" for the URL
+        keyword_encoded = keyword.replace(" ", "%20")
+        # keyword_encoded = keyword_encoded.join(" 2023")
+        
+        # Construct the URL for the API request
+        url = f"https://services.nvd.nist.gov/rest/json/cves/2.0/?pubStartDate={get_yesterday_formatted_date()}&pubEndDate={get_current_formatted_date()}&keywordSearch={keyword_encoded}&cvssV3Severity=HIGH&resultsPerPage=3" if keyword != "" else f"https://services.nvd.nist.gov/rest/json/cves/2.0/?pubStartDate={get_yesterday_formatted_date()}&pubEndDate={get_current_formatted_date()}&cvssV3Severity=HIGH&resultsPerPage=3"
+        print(url)
+        try:
+            # Send the request to the NVD API
+            response = requests.get(url)
+            # Check if the request was successful
+            if response.status_code == 200:
+                # Return the JSON response
+                formatted = format_cve(response.json())
+                return llm.invoke("Summarize the following CVEs:\n\n"+formatted)
+            else:
+                return {"error": "Failed to fetch data from the NVD API.", "status_code": response.status_code}
+        except Exception as e:
+            return {"error": str(e)}
